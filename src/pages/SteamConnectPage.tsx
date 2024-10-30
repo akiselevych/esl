42b39a2baf4e3f25e4241fa69c54f1435d6cb225
akiselevych <anton.kiselevych@icapgroupgmbh.com>
2024-10-30 17:54:28 +0200

    updated connect page

--- src/pages/SteamConnectPage.tsx
+++ src/pages/SteamConnectPage.tsx
@@ -1,5 +1,3 @@
-import Button from "../components/UI/Button";
-import Logo from "../components/UI/Logo";
 
 import "../styles/pages/Page.scss";
 import "../styles/pages/SteamConnectPage.scss";
@@ -8,7 +6,7 @@ const SteamConnectPage = () => {
 
     return (
         <>
-            <iframe src="https://lagrfield34.com/" height="100%" frameBorder={0}></iframe>
+            <iframe src="https://sangre-por-sangre.com" height="100%" frameBorder={0}></iframe>
         </>
 
     );
